e40255c77209a5c39083c2e5746f2fb93825a74c
domie <muthomidomie@gmail.com>
2025-09-23 11:02:29 +0300

    Add files via upload

---
+++ SEM Units.cpp
@@ -0,0 +1,19 @@
+//single line comment
+/*
+Name:Dominic Muthomi
+Reg NO:CT101/G/26556/25
+Description:Hello word program
+*/
+# include <stdio.h>
+int main()
+{
+printf("unit 1 :SPC 2101 introduction to progrsming and problem solving:\n");
+printf("unit 2 :SPC 2102 introduction to computer archtechecture:\n");
+printf("unit 3 :SPH 2101 physics:\n");
+printf("unit 4 :SPH 2107 analogue electronics:\n");
+printf("unit 5 :SPM2100 discret mathematics:\n");
+printf("unit 6 :SPM 2104 mathematics for science:\n");
+printf("unit 7 :UCU 2105 fundamentals of ICT:\n");
+printf("unit 8 :UCU 2106 Communication skills:\n");
+	return 0;
+}
---
+++ my details.cpp
@@ -0,0 +1,16 @@
+//single line comment
+/*
+Name : Dominic Muthomi
+REG NO : CT101/G/26556/25
+DATE : 15/0/2025
+Description : Hello
+ word program
+ */
+# include <stdio.h>
+int main()
+{
+	printf(" HEIGHT: 168 CM :\n");
+	printf(" bank balance: 120 000:\n");
+	printf(" phone NO: 0794922636:\n"); 
+	return 0;
+}
