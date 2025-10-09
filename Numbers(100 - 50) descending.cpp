16aeac2a7a4aa5fae174b81f3e02c7604ff635fa
domie <muthomidomie@gmail.com>
2025-10-09 23:34:41 +0300

    Add files via upload

---
+++ Numbers(100 - 50) descending.cpp
@@ -0,0 +1,15 @@
+/*
+Name : Dominic Muthomi
+Reg NO : CT101/G/26556/25
+Date :  6/10/2025
+Description : A program of numbers 100-50 in descending order
+*/
+
+#include <stdio.h>
+int main()
+{
+	for (int i = 100; i >=50;i--){
+	printf("%d\n", i);
+	}
+return 0;
+}
